acb59beca94f895a69f6a9bc01adc32ab0686b39
Franco Androetto <androettofranco@gmail.com>
2023-09-23 15:20:34 -0300

    delete dist

--- dist/modules/migration/controllers/migration.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { MigrationService } from '../services/migration.service';
-export declare class MigrationController {
-    private readonly migrationService;
-    constructor(migrationService: MigrationService);
-    migrationResource(): Promise<void>;
-}

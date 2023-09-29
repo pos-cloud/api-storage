e1c3b570c8e1d587571a7c624d0ea689b6983bb2
Franco Androetto <androettofranco@gmail.com>
2023-09-28 21:30:00 -0300

    build: js

---
+++ dist/src/common/enums/media.enum.d.ts
@@ -0,0 +1,8 @@
+declare enum ORIGINMEDIA {
+    ARTICLES = "articles",
+    CATEGORIES = "categories",
+    MAKES = "makes",
+    COMPANY = "companies",
+    RESOURCES = "resources"
+}
+export default ORIGINMEDIA;
